14f7fcc08cdb8944642dd7fb37fefb35a7255a39
onny <onny.s@students.amikom.ac.id>
2021-01-09 12:07:53 +0700

    Add getter and setter and add header media

--- TestAPI/ApiHelper.cs
+++ TestAPI/ApiHelper.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace TestAPI
 {
-    class ApiHelper
+    public class ApiHelper
     {
+        public static HttpClient ApiClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            ApiClient = new HttpClient();
+            ApiClient.DefaultRequestHeaders.Accept.Clear();
+            ApiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
     }
 }
